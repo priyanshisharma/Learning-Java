ad566288af7f4f1e97fb70181eb2afcaa50a0600
priyanshisharma <priyanshi417@gmail.com>
2020-07-07 15:56:09 +0530

    HammingDistance and IslandPerimeter

---
+++ Week3/LeetCodePractice/HammingDistance.java
@@ -0,0 +1,15 @@
+package Week3.LeetCodePractice;
+
+public class HammingDistance {
+    public int hammingDistance(int x, int y) {
+        int ctr = 0;
+        while(x>0||y>0)
+        {
+            if(x%2!=y%2)
+                ctr++;
+            x/=2;
+            y/=2;
+        }
+        return ctr;
+    }
+}
---
+++ Week3/LeetCodePractice/IslandPerimeter.java
@@ -0,0 +1,58 @@
+package Week3.LeetCodePractice;
+
+public class IslandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        
+        if(grid.length == 0)
+            return 0;
+        
+        int res=0;
+        
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0; j<grid[i].length;j++)
+            {
+                
+                if(grid[i][j]==1)
+                {
+                    if(i>0)
+                    {
+                       if(grid[i-1][j] == 0)
+                           res++;
+                    }
+                    else
+                        res++;
+
+                    if(i<grid.length-1)
+                    {
+                         if(grid[i+1][j] == 0)
+                           res++;
+                    }
+                    else
+                        res++;
+
+                    if(j>0)
+                    {
+                        if(grid[i][j-1] == 0)
+                           res++;
+                    }
+                    else
+                        res++;
+
+                    if(j<grid[i].length-1)
+                    {
+                        if(grid[i][j+1] == 0)
+                           res++;
+                    }
+                    else
+                        res++;
+                
+                }
+                    
+            }
+                     
+        }
+        
+        return res;
+    }
+}
