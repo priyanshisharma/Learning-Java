201048a4a2a95f267a0f6c8028056b498a4daf7d
priyanshisharma <priyanshi417@gmail.com>
2020-07-10 17:42:28 +0530

    LC practice

---
+++ Week3/LeetCodePractice/SubtractTheProductAndSumofAnIntegeer.java
@@ -0,0 +1,16 @@
+package Week3.LeetCodePractice;
+
+public class SubtractTheProductAndSumofAnIntegeer {
+    public int subtractProductAndSum(int n) {
+        
+        int sum=0, pro=1;
+        while(n>0)
+        {
+            sum+=(n%10);
+            pro*=(n%10);
+            n/=10;
+        }
+        return pro-sum;
+        
+    }
+}
